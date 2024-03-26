a338b0d6b3dd0425a3cc4b0864845bbb49b60804
Amiel Nava <amieln@icloud.com>
2024-03-26 15:27:40 -0700

    Added environment configuration

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+const apiUrl = "http://localhost:5111/api/";
+export const environment = {
+  apiUrl: apiUrl,
+  locationCoordsUrl: apiUrl + "location/coordinates",
+  locationZipcodeUrl: apiUrl + "location/zipcode",
+}
