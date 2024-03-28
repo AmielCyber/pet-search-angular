fea90c64289f6633285e28b7b85a782317badc29
Amiel Nava <amieln@icloud.com>
2024-03-28 16:48:10 -0700

    Update location.service.ts

--- src/app/core/location/location.service.ts
+++ src/app/core/location/location.service.ts
@@ -3,7 +3,7 @@ import {BehaviorSubject, Observable, finalize, catchError, EMPTY} from "rxjs";
 
 import {Location, defaultLocation} from "./location.model";
 import {LocationHttpService} from "./location-http.service";
-import {LocationSnackbarService} from "./location-snackbar.service";
+import {SnackbarService} from "../snackbar/snackbar.service";
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +16,7 @@ export class LocationService {
   private readonly isLoadingSubject = new BehaviorSubject<boolean>(false);
   private previousGeoLocation?: Location;
 
-  constructor(private locationHttpService: LocationHttpService, private locationSnackbarService: LocationSnackbarService) {
+  constructor(private locationHttpService: LocationHttpService, private snackbarService: SnackbarService) {
     this.location$ = this.locationSubject.asObservable();
     this.isLoading$ = this.isLoadingSubject.asObservable();
   }
@@ -26,28 +26,28 @@ export class LocationService {
     this.locationHttpService.getLocationFromZipcode(zipcode)
       .pipe(
         catchError(err => {
-          this.locationSnackbarService.error(err?.message ?? "Failed to update zipcode.")
+          this.snackbarService.error(err?.message ?? "Failed to update zipcode.")
           return EMPTY;
         }),
         finalize(() => this.isLoadingSubject.next(false))
       )
       .subscribe(location => {
         this.locationSubject.next(location)
-        this.locationSnackbarService.success("Updated entered zipcode!")
+        this.snackbarService.success("Updated entered zipcode!")
       });
   }
 
   setLocationFromBrowserGeolocation(): void {
     if (this.previousGeoLocation) {
       this.locationSubject.next(this.previousGeoLocation);
-      this.locationSnackbarService.success("Updated zipcode from previous browser location!")
+      this.snackbarService.success("Updated zipcode from previous browser location!")
     } else if (navigator["geolocation"]) {
       navigator.geolocation.getCurrentPosition(
         (g) => this.setLocationFromGeolocationPosition(g),
-        (e) => this.locationSnackbarService.error(e.message ?? "Failed to retrieve location.")
+        (e) => this.snackbarService.error(e.message ?? "Failed to retrieve location.")
       );
     } else {
-      this.locationSnackbarService.error("Geolocation is not supported by your browser.");
+      this.snackbarService.error("Geolocation is not supported by your browser.");
     }
   }
 
@@ -58,13 +58,13 @@ export class LocationService {
     this.locationHttpService.getLocationFromCoordinates(coords.longitude, coords.latitude)
       .pipe(
         catchError(err => {
-          this.locationSnackbarService.error(err?.message ?? "Failed to retrieve location.");
+          this.snackbarService.error(err?.message ?? "Failed to retrieve location.");
           return EMPTY;
         }),
         finalize(() => this.isLoadingSubject.next(false)),
       )
       .subscribe(location => {
-        this.locationSnackbarService.success("Updated local zipcode!");
+        this.snackbarService.success("Updated local zipcode!");
         this.locationSubject.next(location)
         this.previousGeoLocation = location;
       });
