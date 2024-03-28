3e6ceedfeb28453924081ae3a634a3e399469dd6
Amiel Nava <amieln@icloud.com>
2024-03-28 16:50:52 -0700

    Toggle Dark/Light Theme

--- src/app/core/toggle-theme-button/toggle-theme-button.component.ts
+++ src/app/core/toggle-theme-button/toggle-theme-button.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-toggle-theme-button',
   templateUrl: './toggle-theme-button.component.html',
-  styleUrl: './toggle-theme-button.component.sass'
 })
 export class ToggleThemeButtonComponent {
+  isDarkMode = true;
+  private readonly darkClass = "theme-dark";
+  private readonly lightClass = "theme-light";
+
+  constructor(private renderer: Renderer2) { }
+
+  onToggleTheme(){
+    this.isDarkMode? this.setLightTheme() : this.setDarkTheme();
+  }
+
+  private setDarkTheme(){
+    this.renderer.addClass(document.body, this.darkClass);
+    this.renderer.removeClass(document.body, this.lightClass);
+    this.isDarkMode = true;
+  }
+  private setLightTheme(){
+    this.renderer.addClass(document.body, this.lightClass);
+    this.renderer.removeClass(document.body, this.darkClass);
+    this.isDarkMode = false;
+  }
 
 }
