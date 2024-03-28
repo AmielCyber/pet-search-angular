bda8c8f564c2114690711fc9e2875fcecc7004bb
Amiel Nava <amieln@icloud.com>
2024-03-28 16:47:19 -0700

    Browser Location Button

--- src/app/core/location/locate-me-button/locate-me-button.component.ts
+++ src/app/core/location/locate-me-button/locate-me-button.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import {Observable} from "rxjs";
+
+import {LocationService} from "../location.service";
 
 @Component({
   selector: 'app-locate-me-button',
   templateUrl: './locate-me-button.component.html',
-  styleUrl: './locate-me-button.component.sass'
 })
-export class LocateMeButtonComponent {
+export class LocateMeButtonComponent{
+
+  isLoading$: Observable<boolean>;
+  constructor(private locationService: LocationService) {
+    this.isLoading$ = locationService.isLoading$;
+  }
+
+  onBrowserLocation(){
+    this.locationService.setLocationFromBrowserGeolocation();
+  }
 
 }
