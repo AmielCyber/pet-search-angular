d015bbde7ba024e25674562a5f1adeaf13d3c8d2
Amiel Nava <amieln@icloud.com>
2024-03-29 16:21:40 -0700

    Format location-http-service

--- src/app/core/location/location-http.service.ts
+++ src/app/core/location/location-http.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
+import {catchError, Observable, throwError} from "rxjs";
 import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
 
 import {environment} from "../../../environments/environment.development";
-import {catchError, Observable, throwError} from "rxjs";
 import {Location} from "./location.model";
 
 @Injectable({
