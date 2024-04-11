ed07e5f395935a4b36aa8ed4af04a535805e5bd4
Amiel Nava <amieln@icloud.com>
2024-04-11 16:22:03 -0700

    Updated pet search header

--- src/app/pet-search/pet-search-header/pet-search-header.component.ts
+++ src/app/pet-search/pet-search-header/pet-search-header.component.ts
@@ -1,12 +1,21 @@
-import {Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {LocationService} from "../../core/location/location.service";
+import {Observable} from "rxjs";
+
+import {Location} from "../../models/location.model";
 
 @Component({
   selector: 'app-pet-search-header',
   templateUrl: './pet-search-header.component.html',
-  styleUrl: './pet-search-header.component.sass'
+  styleUrl: './pet-search-header.component.sass',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PetSearchHeaderComponent {
-  @Input({required: true}) petTypePlural!: string;
-  @Input({required: true}) locationName!: string;
-  @Input({required: true}) totalCount!: number;
+  @Input({required: true}) petTypePlural?: string;
+  @Input({required: true}) zipcode?: string;
+  @Input({required: true}) totalCount?: number;
+  location$: Observable<Location> = this.locationService.location$;
+
+  constructor(private locationService: LocationService) {
+  }
 }
