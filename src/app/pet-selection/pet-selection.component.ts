d4e76a4646ec11c77f7c335079bcf21e4c67e9ea
Amiel Nava <amieln@icloud.com>
2024-04-18 16:31:52 -0700

    Update pet-selection.component.ts

--- src/app/pet-selection/pet-selection.component.ts
+++ src/app/pet-selection/pet-selection.component.ts
@@ -3,7 +3,7 @@ import {Observable} from "rxjs";
 
 import {Location} from "../models/location.model";
 import {PetResource} from "../shared/pet-resource.model";
-import {HttpRequestStateModel} from "../shared/http-request-state.model";
+import {HttpRequestState} from "../shared/http-request-state.model";
 import {ROUTER_TOKENS} from "../app.routes";
 import {PetIconService} from "../shared/pet-icon/pet-icon.service";
 import {LocationService} from "../core/location/location.service";
@@ -15,7 +15,7 @@ import {LocationService} from "../core/location/location.service";
 })
 export class PetSelectionComponent {
   readonly ROUTER_TOKENS = ROUTER_TOKENS;
-  readonly locationData$: Observable<HttpRequestStateModel<Location>>;
+  readonly locationData$: Observable<HttpRequestState<Location>>;
   readonly petResourceList: PetResource[];
 
   constructor(private petIconService: PetIconService, private locationService: LocationService) {
