5fef13dcc63eff2f42e55aa6da2560c5820ceca1
Amiel Nava <amieln@icloud.com>
2024-04-26 14:16:37 -0700

    Set initial theme to browser's prefer theme

--- src/app/core/location-buttons/toggle-theme-button/toggle-theme-button.component.ts
+++ src/app/core/location-buttons/toggle-theme-button/toggle-theme-button.component.ts
@@ -1,29 +1,36 @@
-import {Component, Renderer2} from '@angular/core';
+import {Component, OnInit, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-toggle-theme-button',
   templateUrl: './toggle-theme-button.component.html',
 })
-export class ToggleThemeButtonComponent {
+export class ToggleThemeButtonComponent implements OnInit {
   isDarkMode = true;
   private readonly darkClass = "theme-dark";
   private readonly lightClass = "theme-light";
 
-  constructor(private renderer: Renderer2) { }
-
-  onToggleTheme(){
-    this.isDarkMode? this.setLightTheme() : this.setDarkTheme();
+  constructor(private renderer: Renderer2) {
   }
 
-  private setDarkTheme(){
-    this.renderer.addClass(document.body, this.darkClass);
-    this.renderer.removeClass(document.body, this.lightClass);
-    this.isDarkMode = true;
-  }
-  private setLightTheme(){
-    this.renderer.addClass(document.body, this.lightClass);
-    this.renderer.removeClass(document.body, this.darkClass);
-    this.isDarkMode = false;
+  ngOnInit(): void {
+    if (window.matchMedia("prefers-color-scheme").media !== "not all") {
+      // Browser supports prefers-color-scheme
+      this.isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    }
+    this.setTheme(this.isDarkMode);
   }
 
+  onToggleTheme() {
+    const isDarkMode = !this.isDarkMode;
+    this.setTheme(isDarkMode);
+  }
+
+  private setTheme(isDarkMode: boolean): void {
+    const newTheme = isDarkMode ? this.darkClass : this.lightClass;
+    const oldTheme = isDarkMode ? this.lightClass : this.darkClass;
+
+    this.renderer.addClass(document.body, newTheme);
+    this.renderer.removeClass(document.body, oldTheme);
+    this.isDarkMode = isDarkMode;
+  }
 }
