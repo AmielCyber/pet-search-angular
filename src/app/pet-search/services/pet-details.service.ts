3c5196be461d3b6e8871943aec2ac89689151ba8
Amiel Nava <amieln@icloud.com>
2024-05-01 12:18:18 -0700

    Moved to share services

--- src/app/pet-search/services/pet-details.service.ts
+++ src/app/pet-search/services/pet-details.service.ts
@@ -1,26 +1,38 @@
 import {Injectable, SecurityContext} from '@angular/core';
 import {DomSanitizer} from "@angular/platform-browser";
-import {catchError, concat, map, Observable, of, share, startWith, switchMap, tap} from "rxjs";
+import {catchError, map, Observable, of, startWith, tap} from "rxjs";
 
-import {PetHttpService} from "../shared/services/pet-http.service";
-import {HttpRequestState} from "../core/models/http-request-state.model";
-import {Pet} from "../core/models/pet.model";
+import {PetHttpService} from "../../shared/services/pet-http.service";
+import {HttpRequestState} from "../../core/models/http-request-state.model";
+import {Pet} from "../../core/models/pet.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PetDetailsService {
+  private cachedPetDetails?: HttpRequestState<Pet>;
 
   constructor(private petHttpService: PetHttpService, private sanitizer: DomSanitizer) {
   }
 
   getPet(petId: number): Observable<HttpRequestState<Pet>> {
+    if (this.cachedPetDetails && this.cachedPetDetails.data?.id === petId) {
+      return of(this.cachedPetDetails);
+    }
     return this.petHttpService.getPet(petId).pipe(
+      tap(p => this.cachedPetDetails = {isLoading: false, data: p}),
       map(p => this.getPetHttpRequestState(p)),
       startWith({isLoading: true}),
       catchError(err => of({isLoading: false, error: err.error})
       )
     );
+  }
+
+  setCachedPet(pet: Pet): void {
+    this.cachedPetDetails = {
+      isLoading: false,
+      data: pet,
+    }
   }
 
   private getPetHttpRequestState(pet: Pet): HttpRequestState<Pet> {
