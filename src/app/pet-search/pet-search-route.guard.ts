dd3501e1ef60272acdd5dec6d4b6dcacf5b30570
Amiel Nava <amieln@icloud.com>
2024-12-26 12:26:06 -0800

    Pet Search Guard Tests

--- src/app/pet-search/pet-search-route.guard.ts
+++ src/app/pet-search/pet-search-route.guard.ts
@@ -1,21 +1,21 @@
-import {CanActivateFn, Router} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot} from '@angular/router';
 import {inject} from "@angular/core";
 
 import {ROUTER_TOKENS} from "../routes/router-tokens.model";
 import {PetSearchParamsService} from "./services/pet-search-params.service";
 import {ErrorPageService} from "../core/services/error-page.service";
 
-export const petSearchRouteGuard: CanActivateFn = (route, state) => {
+export const petSearchRouteGuard: CanActivateFn = (route: ActivatedRouteSnapshot, _: RouterStateSnapshot): boolean => {
   const petSearchParamsService = inject(PetSearchParamsService);
   const router = inject(Router);
   const errorPageService = inject(ErrorPageService);
 
 
   const petTypePlural: string | null = route.paramMap.get("petTypePlural");
-  const isValidPetType = petSearchParamsService.isValidPetType(petTypePlural);
+  const isValidPetType: boolean = petSearchParamsService.isValidPetType(petTypePlural);
   if (!isValidPetType) {
     errorPageService.setErrorMessage(`Pet type '${petTypePlural}' not supported.`);
-    router.navigate([ROUTER_TOKENS.ERROR]);
+    router.navigate([ROUTER_TOKENS.ERROR]).then(r => r);
   }
 
   return isValidPetType;
